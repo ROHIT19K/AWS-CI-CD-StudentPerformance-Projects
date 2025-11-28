773487cadc8af652175a9410f3fa10bf9335487a
ROHIT19K <rohitk26311@outlook.com>
2025-11-28 23:25:17 +0530

    Logging and Exception

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,5 @@
 import sys 
-import logging 
-import os 
-from datetime import datetime 
-## from src.logger import logging
+import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -20,4 +17,6 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+
     
